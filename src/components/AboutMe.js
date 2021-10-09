80e892f1680abe077421f08dec07dd7566464f5d
fafaew <rafael.gomest@gmail.com>
2021-10-09 09:58:44 -0300

    about-me background change

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -10,33 +10,35 @@ import {
 
 const AboutMe = () => {
   return (
-    <div id="about" className="container py-5">
-      <div className="row">
-        <div className="col-lg-6 col-xm-12">
-          <div className="photo-wrap mb-5">
-            <img className="profile-img" src={author} alt="author..." />
+    <div className="about-me">
+      <div id="about" className="container py-5">
+        <div className="row">
+          <div className="col-lg-6 col-xm-12">
+            <div className="photo-wrap mb-5">
+              <img className="profile-img" src={author} alt="author..." />
+            </div>
           </div>
-        </div>
-        <div className="col-lg-6 col-xm-12">
-          <h1 className="about-heading">about me</h1>
-          <p>
-            Hello everyone! I'm Rafael Gomes, a web developer from Minas Gerais,
-            Brazil. I have been developing websites for over 2 years.
-          </p>
-          <p>Apart from coding, some other activities that I love to do:</p>
-          <div className="about-marker"></div>
-          <p className="about-paragraph">Play Video Games</p>
-          <div className="about-marker"></div>
-          <p className="about-paragraph">Practice Sports</p>
-          <div className="about-marker"></div>
-          <p className="about-paragraph">Learn about financial market</p>
-          <br />
-          <p>Technologies that I use:</p>
-          <div className="aboutIcons">
-            <FontAwesomeIcon className="icon" icon={faPhp} size="2x" />
-            <FontAwesomeIcon className="icon" icon={faNode} size="2x" />
-            <FontAwesomeIcon className="icon" icon={faJsSquare} size="2x" />
-            <FontAwesomeIcon className="icon" icon={faReact} size="2x" />
+          <div className="col-lg-6 col-xm-12">
+            <h1 className="about-heading">about me</h1>
+            <p>
+              Hello everyone! I'm Rafael Gomes, a web developer from Minas Gerais,
+              Brazil. I have been developing websites for over 2 years.
+            </p>
+            <p>Apart from coding, some other activities that I love to do:</p>
+            <div className="about-marker"></div>
+            <p className="about-paragraph">Play Video Games</p>
+            <div className="about-marker"></div>
+            <p className="about-paragraph">Practice Sports</p>
+            <div className="about-marker"></div>
+            <p className="about-paragraph">Learn about financial market</p>
+            <br />
+            <p>Technologies that I use:</p>
+            <div className="aboutIcons">
+              <FontAwesomeIcon className="icon" icon={faPhp} size="2x" />
+              <FontAwesomeIcon className="icon" icon={faNode} size="2x" />
+              <FontAwesomeIcon className="icon" icon={faJsSquare} size="2x" />
+              <FontAwesomeIcon className="icon" icon={faReact} size="2x" />
+            </div>
           </div>
         </div>
       </div>

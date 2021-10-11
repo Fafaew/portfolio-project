5f15ce01e6dc4aed9131c3bd49d3682b10654dd3
fafaew <rafael.gomest@gmail.com>
2021-10-11 11:10:57 -0300

    Experience: fix headings bugs and update text description

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -30,8 +30,10 @@ const Experience = () => {
         <h3 className="vertical-timeline-element-title">
           Started my carrer as a Software Developer
         </h3>
-        <h4 className="vertical-timeline-element-subtitle"></h4>
-        <p>personal projects and worked as a freelancer</p>
+        <p>Personal projects and worked as a freelancer
+          <br />
+          PHP, JavaScript
+        </p>
       </VerticalTimelineElement>
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
@@ -68,92 +70,6 @@ const Experience = () => {
         <p>PHP, Drupal and JavaScript</p>
       </VerticalTimelineElement>
     </VerticalTimeline>
-
-    //     <div id="experience" className="experience">
-    //       <div className="d-flex justify-content-center my-5">
-    //         <h1>Experience</h1>
-    //       </div>
-    //       <div className="container experience-wrapper">
-    //         <div className="timeline-block timeline-block-right">
-    //           <div className="marker"></div>
-    //           <div className="timeline-content">
-    //             <h3>2019</h3>
-    //             <p>
-    //               Start my carrer as developer, creating personal projects and
-    //               working as a freelancer
-    //             </p>
-    //           </div>
-    //         </div>
-    //         {/* - */}
-    //         <div className="timeline-block timeline-block-left">
-    //           <div className="marker"></div>
-    //           <div className="timeline-content">
-    //             <h3>2020-2021</h3>
-    //             <p>Worked with PHP and JavaScript</p>
-    //           </div>
-    //         </div>
-    //         {/* - */}
-    //         <div className="timeline-block timeline-block-right">
-    //           <div className="marker"></div>
-    //           <div className="timeline-content">
-    //             <h3>2016-2017</h3>
-    //             <p>
-    //               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit
-    //               tempora sint voluptates veritatis officia, omnis facilis
-    //               asperiores magnam esse explicabo repellat quisquam rem
-    //               consequuntur consectetur cumque doloribus porro quia accusamus.
-    //               Aperiam voluptatem labore unde quidem necessitatibus, consequatur
-    //               assumenda nam eveniet.
-    //             </p>
-    //           </div>
-    //         </div>
-    //         {/* - */}
-    //         <div className="timeline-block timeline-block-left">
-    //           <div className="marker"></div>
-    //           <div className="timeline-content">
-    //             <h3>2018-2019</h3>
-    //             <p>
-    //               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit
-    //               tempora sint voluptates veritatis officia, omnis facilis
-    //               asperiores magnam esse explicabo repellat quisquam rem
-    //               consequuntur consectetur cumque doloribus porro quia accusamus.
-    //               Aperiam voluptatem labore unde quidem necessitatibus, consequatur
-    //               assumenda nam eveniet.
-    //             </p>
-    //           </div>
-    //         </div>
-    //         {/* - */}
-    //         <div className="timeline-block timeline-block-right">
-    //           <div className="marker"></div>
-    //           <div className="timeline-content">
-    //             <h3>2014-2015</h3>
-    //             <p>
-    //               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit
-    //               tempora sint voluptates veritatis officia, omnis facilis
-    //               asperiores magnam esse explicabo repellat quisquam rem
-    //               consequuntur consectetur cumque doloribus porro quia accusamus.
-    //               Aperiam voluptatem labore unde quidem necessitatibus, consequatur
-    //               assumenda nam eveniet.
-    //             </p>
-    //           </div>
-    //         </div>
-    //         {/* - */}
-    //         <div className="timeline-block timeline-block-left">
-    //           <div className="marker"></div>
-    //           <div className="timeline-content">
-    //             <h3>2018-2020</h3>
-    //             <p>
-    //               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit
-    //               tempora sint voluptates veritatis officia, omnis facilis
-    //               asperiores magnam esse explicabo repellat quisquam rem
-    //               consequuntur consectetur cumque doloribus porro quia accusamus.
-    //               Aperiam voluptatem labore unde quidem necessitatibus, consequatur
-    //               assumenda nam eveniet.
-    //             </p>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
   );
 };
 

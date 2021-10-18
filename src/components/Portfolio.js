a0803e2cfac72bed2aa5ef6a487471b60bc910a6
fafaew <rafael.gomest@gmail.com>
2021-10-18 11:38:08 -0300

    Portfolio: added dashboard project and icons for live demo

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,6 +4,8 @@ import { faGithubAlt } from '@fortawesome/free-brands-svg-icons';
 import postit from '../images/imgPortfolio/postit.jpg';
 import gymImg from '../images/imgPortfolio/gymImg.jpg';
 import develop from '../images/imgPortfolio/Develop.jpg';
+import dashboard from '../images/imgPortfolio/dashboard.jpg';
+import { faLaptop } from '@fortawesome/free-solid-svg-icons';
 
 const Portfolio = () => {
   return (
@@ -11,24 +13,27 @@ const Portfolio = () => {
       <h1 className="py-5">Portfolio</h1>
       <div className="container">
         <div className="row">
-          <div className=" col-lg-3 col-md-6 col-sm-6">
+        <div className=" col-lg-3 col-md-6 col-sm-6">
             <div className="box">
               <div className="portfolio-image">
-                <img src={postit} alt="task hand" />
+                <img src={dashboard} alt="dashboard" />
               </div>
-              <h3>Task Tracker</h3>
+              <h3>Dashboard</h3>
               <p>
-                It's a single place where you and your team can organize and
-                prioritize every single task or item that needs to get done
-              </p>  
+              A admnistrative panel that contains metrics and indicators. That numbers will help the achievement of organizational goals
+              </p>
               <div className="portfolioIcons" >
-                <a href="https://github.com/Fafaew/ReactTraskTracker" rel="noreferrer" target="_blank">
+                <a href="https://github.com/Fafaew/react-dashboard" rel="noreferrer" target="_blank">
                 <FontAwesomeIcon icon={faGithubAlt} size="2x" />
                 <p>Github</p>
                 </a>
-              </div>             
-            </div>                 
-          </div>
+                <a href="https://fafaew.github.io/react-dashboard" rel="noreferrer" target="_blank">
+                <FontAwesomeIcon icon={faLaptop} size="2x" />
+                <p>Live Demo</p>
+                </a>
+              </div> 
+            </div>
+          </div>         
           <div className=" col-lg-3 col-md-6 col-sm-6">
             <div className="box">
               <div className="portfolio-image">
@@ -44,17 +49,35 @@ const Portfolio = () => {
                 <FontAwesomeIcon icon={faGithubAlt} size="2x" />
                 <p>Github</p>
                 </a>
+                <a href="https://fafaew.github.io/react-dashboard" rel="noreferrer" target="_blank">
+                <FontAwesomeIcon icon={faLaptop} size="2x" />
+                <p>Live Demo</p>
+                </a>
               </div> 
             </div>
           </div>
           <div className=" col-lg-3 col-md-6 col-sm-6">
             <div className="box">
               <div className="portfolio-image">
-                <img src={develop} alt="computer screen" />
+                <img src={postit} alt="task hand" />
               </div>
-              <h3>In Progress</h3>
-            </div>
-          </div>
+              <h3>Task Tracker</h3>
+              <p>
+                It's a single place where you and your team can organize and
+                prioritize every single task or item that needs to get done
+              </p>  
+              <div className="portfolioIcons" >
+                <a href="https://github.com/Fafaew/ReactTraskTracker" rel="noreferrer" target="_blank">
+                <FontAwesomeIcon icon={faGithubAlt} size="2x" />
+                <p>Github</p>
+                </a>
+                <a href="https://fafaew.github.io/react-dashboard" rel="noreferrer" target="_blank">
+                <FontAwesomeIcon icon={faLaptop} size="2x" />
+                <p>Live Demo</p>
+                </a>
+              </div>             
+            </div>                 
+          </div>         
           <div className=" col-lg-3 col-md-6 col-sm-6">
             <div className="box">
               <div className="portfolio-image">
